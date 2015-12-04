07f4c01858c8ee07fbfc9c6771813cd6283616be
xjchuan <627538782@qq.com>
2015-12-04 16:13:44 +0800

    2015年12月4日16:13:38

--- app/src/main/java/fruitbasket/com/bodyfit/data/DataBuffer.java
+++ app/src/main/java/fruitbasket/com/bodyfit/data/DataBuffer.java
@@ -32,7 +32,32 @@ public class DataBuffer {
     }
 
     public void transferData(){
-        ////
+        if(buffer==null)
+            return;
+        int bufferLength = buffer.size();
+
+        int totalLength = 0;
+        for(int i = 0; i<bufferLength; i++){
+            totalLength += buffer.get(i).getAxSet().length;
+        }
+
+        SourceData [] sourceData = new SourceData[totalLength];
+        int count = 0;
+        for(int i = 0; i<bufferLength; i++){
+            Data item = buffer.get(i);
+            for(int j = 0; j<item.getAxSet().length; j++){
+                sourceData[count++] = new SourceData(
+                        null,
+                        item.getAxSet()[j],
+                        item.getAySet()[j],
+                        item.getAzSet()[j],
+                        item.getGxSet()[j],
+                        item.getGySet()[j],
+                        item.getGzSet()[j]);
+            }
+        }
+
+        dataSet = new Data(sourceData);
         buffer.clear();
     }
 
