cac47217fe1a97d022442ba7d54dc10fe083d40a
CielWu <871447603@qq.com>
2016-11-21 14:13:48 +0800

    modified GroupExerciseScore.java,add GroupExerciseScore.java

--- app/src/main/java/fruitbasket/com/bodyfit/analysis/GroupExerciseScore.java
+++ app/src/main/java/fruitbasket/com/bodyfit/analysis/GroupExerciseScore.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import fruitbasket.com.bodyfit.data.DataSet;
 import fruitbasket.com.bodyfit.data.DataSetBuffer;
+import fruitbasket.com.bodyfit.data.GroupDataSetBuffer;
 import fruitbasket.com.bodyfit.data.SelectedDataSet;
 
 /**
@@ -12,18 +13,13 @@ import fruitbasket.com.bodyfit.data.SelectedDataSet;
 public class GroupExerciseScore {
     public static final String TAG="GroupExerciseScore";
 
-    /////beginActionIndex,finishActionIndex要在一组结束后重置为0
-    private int beginActionIndex=0; //在数组中，每一个开始做动作的指针
-    private int beginArray[];
-    private int finishActionIndex=0;    //在数组中，每一个结束做动作的指针
-    private int finishArray[];
-
+    GroupDataSetBuffer groupBuffer; //储存一整组的数据，运动+静止+运动+静止+...,第一个动作之前的数据不会储存
     private DataSetBuffer dataBuffer;   //储存运动数据
     private SelectedDataSet selectedData;   //选择需要的维度之后储存
-
     private DynamicTimeWarping dtw;     //模式匹配算法类对象
 
     GroupExerciseScore(){
+        groupBuffer=new GroupDataSetBuffer();
         dataBuffer=new DataSetBuffer();
         selectedData=new SelectedDataSet();
         dtw=new DynamicTimeWarping();
@@ -39,5 +35,12 @@ public class GroupExerciseScore {
         return true;
     }
 
-    public add
+    public void finishAction(){
+        groupBuffer.add(dataBuffer);
+    }
+
+    public void finishStatic(){
+        groupBuffer.add(dataBuffer);
+    }
+
 }
